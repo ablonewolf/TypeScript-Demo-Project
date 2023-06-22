a01f64132cc6b00a103212ce3518e011d0bd83a5
Arka Bhuiyan <arkabhuiyan66@gmail.com>
2023-06-22 15:47:48 +0600

    created function to add and an object type.

--- app.ts
+++ app.ts
@@ -1,1 +1,15 @@
-console.log('Your code goes here...');
+// console.log('Your code goes here...');
+
+function add(number1: number, number2: number) {
+  return number1 + number2;
+}
+
+type person = {
+  name: string;
+  age: number;
+};
+
+const arka: person = {
+  name: 'Arka',
+  age: 25
+};
