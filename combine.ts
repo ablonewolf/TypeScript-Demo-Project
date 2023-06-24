77b877b0a2aaa00b16ebdc5eb44a79072ef44364
Arka Bhuiyan <arkabhuiyan66@gmail.com>
2023-06-24 16:51:26 +0600

    demonstrated the use of a callback function.

--- combine.ts
+++ combine.ts
@@ -2,6 +2,17 @@ type combinable = number | string;
 type conversionDescriptor = 'number' | 'text';
 
 const add = (number1: number, number2: number) => number1 + number2;
+const addAndHandle = (
+  input1: combinable,
+  input2: combinable,
+  callBackFunction: (output: combinable) => void
+) => {
+  const inputType =
+    typeof input1 === 'number' && typeof input2 === 'number'
+      ? 'number'
+      : 'text';
+  callBackFunction(combine(input1, input2, inputType));
+};
 
 const combine = function (
   input1: combinable,
@@ -18,6 +29,8 @@ const combine = function (
     return input1.toString() + ' ' + input2.toString();
   }
 };
-
-console.log(combine(56, 44, 'number'));
-console.log(combine('Hello', 24, 'text'));
+const printResult = (output: combinable) =>
+  console.log(`The output is: ${output}.`);
+addAndHandle(56, 44, printResult);
+addAndHandle('Hello', 'Arka', printResult);
+addAndHandle('My age is', 24, printResult);
