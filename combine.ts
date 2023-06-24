ff3ed92db754050d6d87324f6fb130ee41d68ff4
Arka Bhuiyan <arkabhuiyan66@gmail.com>
2023-06-24 15:38:05 +0600

    demonstrated the use of literals in function parameters.

--- combine.ts
+++ combine.ts
@@ -1,10 +1,18 @@
-const combine = function (input1: number | string, input2: number | string) {
-  if (typeof input1 === 'number' && typeof input2 === 'number') {
+const combine = function (
+  input1: number | string,
+  input2: number | string,
+  resultConvertion: 'number' | 'text'
+) {
+  if (
+    typeof input1 === 'number' &&
+    typeof input2 === 'number' &&
+    resultConvertion === 'number'
+  ) {
     return input1 + input2;
   } else {
     return input1.toString() + ' ' + input2.toString();
   }
 };
 
-console.log(combine(56, 44));
-console.log(combine('Hello', 24));
+console.log(combine(56, 44, 'number'));
+console.log(combine('Hello', 24, 'text'));
