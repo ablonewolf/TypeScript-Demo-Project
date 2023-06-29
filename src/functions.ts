ad5fcfcf4db032b449b74b64998a78ca6d459fb9
Arka Bhuiyan <arkabhuiyan66@gmail.com>
2023-06-30 01:49:36 +0600

    demonstrated the use of for of loop along with rest operator.

--- src/functions.ts
+++ src/functions.ts
@@ -3,4 +3,12 @@ const addNumbers = (a: number, b: number) => a + b;
 const printOutput: (a: number | string) => void = (output) =>
   console.log(output);
 
+const sum = (...numbers: number[]) => {
+  let sum = 0;
+  for (let number of numbers) {
+    sum += number;
+  }
+  return sum;
+};
+
 printOutput(addNumbers(5, 7));
