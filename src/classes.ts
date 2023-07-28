dddd0fa9110ccc695b5781342b7bd71cd1de63cd
Arka Bhuiyan <arkabhuiyan66@gmail.com>
2023-07-28 17:34:29 +0600

    demonstrated the use of inheritance.

--- src/classes.ts
+++ src/classes.ts
@@ -34,6 +34,18 @@ class Student {
   }
 }
 
+class SoftwareEngineering extends Department {
+  constructor(private chairman: string) {
+    super('Software Engineering');
+    this.chairman = chairman;
+  }
+
+  public describe(this: SoftwareEngineering) {
+    super.describe();
+    console.log(`Chairman of this department: ${this.chairman}`);
+  }
+}
+
 const arka = new Student('Arka Bhuiyan', 26);
 const farhan = new Student('Farhan Zaman', 26);
 const nipa = new Student('Nipa Howlader', 26);
@@ -46,3 +58,20 @@ computerScience.addStudent(nipa);
 computerScience.addStudent(shourov);
 computerScience.describe();
 computerScience.printStudentInfo();
+console.log(`\n`);
+
+const softwareEngineerig = new SoftwareEngineering('Sakib Hasan');
+
+const ishtiaque = new Student('Ishtiaque Habib', 25);
+const anim = new Student('Nafis Imtiyaz Anim', 27);
+const rejaur = new Student('Rejaur Rahman', 26);
+const ifti = new Student('Iftekhar Alam', 26);
+const faiaz = new Student('Faiaz Mursalin', 26);
+
+softwareEngineerig.addStudent(ishtiaque);
+softwareEngineerig.addStudent(anim);
+softwareEngineerig.addStudent(rejaur);
+softwareEngineerig.addStudent(ifti);
+softwareEngineerig.addStudent(faiaz);
+softwareEngineerig.describe();
+softwareEngineerig.printStudentInfo();
